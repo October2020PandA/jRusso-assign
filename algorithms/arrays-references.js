fe459833ca57ab664ef43677c3a48e1b49e9deb8
joshmrusso <joshmrusso@gmail.com>
2020-10-02 19:35:32 -0400

    To Do 2

--- algorithms/arrays-references.js
+++ algorithms/arrays-references.js
@@ -13,5 +13,68 @@ function reverse(arr) {
 
 // Rotate
 function rotate(arr, shiftBy) {
-    
+    console.log(arr);
+    shiftBy = shiftBy % arr.length;
+    if (shiftBy < 0) {
+        shiftBy = arr.length + shiftBy;
+    }
+    var j = 0, k = 0, l = arr[j], m = '';
+    if (arr.length % 2 == 0 && shiftBy % 2 ==0) {
+        var j2 = 1, k2 = 0, l2 = arr[j2], m2 = '';
+        for (var i = 0; i < arr.length; i = i + 2) {
+            k = (j + shiftBy) % arr.length;
+            k2 = (j2 + shiftBy) % arr.length;
+            m = arr[k];
+            m2 = arr[k2];
+            arr[k2] = l2;
+            arr[k] = l;
+            j2 = k2;
+            j = k;
+            l2 = m2;
+            l = m;
+        }
+    } else {
+        for (var i = 0; i < arr.length; i++) {
+            k = (j + shiftBy) % arr.length;
+            m = arr[k];
+            arr[k] = l;
+            j = k;
+            l = m;
+        }
+    }
+    console.log(arr);
 }
+
+// rotate(testArr, 4);
+
+// Filter Range
+function filterRange(arr, min, max) {
+    for (var i = arr.length - 1; i > -1; i--) {
+        if (arr[i] < min) {
+            delete arr[i];
+            arr.length = arr.length - 1;
+        } else if (arr[i] > max) {
+            delete arr[i];
+            arr.length = arr.length - 1;
+        }
+    }
+}
+
+// filterRange(testArr, 2, 5);
+// console.log(testArr);
+
+// Concat
+function arrConcat(arr1, arr2) {
+    var outputArr = [];
+    for (var i = 0; i < arr1.length; i++) {
+        outputArr.push(arr1[i]);
+    }
+    for (var i = 0; i < arr2.length; i++) {
+        outputArr.push(arr2[i]);
+    }
+    return outputArr;
+}
+
+// testArr2 = [3, 5, 1];
+// concatArr = arrConcat(testArr, testArr2);
+// console.log(concatArr);
---
+++ projects/testmaker/logreg/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render, redirect, HttpResponse
+from django.contrib import messages
+from .models import User
+import bcrypt
+
+# Create your views here.
+def index(request):
+    if 'login_id' in request.session:
+        return redirect('/')
+    context = {
+        'title': 'Test Maker App - Welcome',
+    }
+    return render(request, 'logreg.html', context)
+
+def register(request):
+    errors = User.objects.user_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value, extra_tags=key)
+        return redirect('/')
+    hashed_pw = bcrypt.hashpw(request.POST['pw'].encode(), bcrypt.gensalt()).decode()
+    User.objects.create(first_name=request.POST['first_name'], last_name=request.POST['last_name'], email=request.POST['email_address'],  pw=hashed_pw)
+    request.session['login_id'] = User.objects.get(email=request.POST['email_address']).id
+    return redirect('/')
+
+def login(request):
+    errors = User.objects.login_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value, extra_tags=key)
+        return redirect('/')
+    user = User.objects.get(email=request.POST['email_address_lg'])
+    request.session['login_id'] = user.id
+    return redirect('/')
+
+def logout(request):
+    if 'login_id' in request.session:
+        del request.session['login_id']
+    return redirect('/')
